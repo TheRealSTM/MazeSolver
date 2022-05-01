9be0fbdc081fd2b7401282b102fa923d93ffa939
Sean Montague <stmontague1@gmail.com>
2022-05-01 12:32:07 -0700

    Implement the solution exists task using DFS

--- src/com/company/models/MazePosition.java
+++ src/com/company/models/MazePosition.java
@@ -2,13 +2,45 @@ package com.company.models;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.RequiredArgsConstructor;
 
-@RequiredArgsConstructor
 @AllArgsConstructor
 @Data
 public class MazePosition {
-    final private int rowPos;
-    final private int columnPos;
-    private StringBuilder path = new StringBuilder();
+    private int rowPos;
+    private int columnPos;
+    private StringBuilder path;
+
+    public MazePosition(int rowPos, int columnPos) {
+        this(rowPos, columnPos, new StringBuilder());
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == this) return true;
+        if (!(o instanceof MazePosition)) return false;
+        MazePosition other = (MazePosition) o;
+        if (!other.canEqual((Object)this)) return false;
+        if (this.rowPos != other.getRowPos()) return false;
+        if (this.columnPos != other.getColumnPos()) return false;
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        final int PRIME = 59;
+        int result = 1;
+        result = (result * PRIME) + this.rowPos;
+        result = (result * PRIME) + this.columnPos;
+        return result;
+    }
+
+    protected boolean canEqual(Object other) {
+        return other instanceof MazePosition;
+    }
+
+    public void update(MazePosition currentPosition) {
+        this.rowPos = currentPosition.getRowPos();
+        this.columnPos = currentPosition.getColumnPos();
+        this.path = currentPosition.getPath();
+    }
 }
--- src/com/company/taskSolvers/SolutionExistsSolver.java
+++ src/com/company/taskSolvers/SolutionExistsSolver.java
@@ -1,9 +1,40 @@
 package com.company.taskSolvers;
 
+import com.company.constants.MazeGridDirectionConstants;
 import com.company.models.Maze;
+import com.company.models.MazePosition;
+import com.company.models.MazePositionChange;
+import com.company.utilities.MazePositionUtility;
+
+import java.util.HashSet;
+import java.util.Set;
 
 public class SolutionExistsSolver implements MazeSolver {
     public String solveMaze(Maze maze) {
-        return "The maze was solved.";
+        MazePosition start = maze.getStartPos(), end = maze.getEndPos();
+        Set<MazePosition> visitedPositions = new HashSet<>();
+        MazePosition finalPosition = new MazePosition(-1, -1, new StringBuilder());
+        searchMaze(maze, start, visitedPositions, finalPosition);
+        return "The solution to the maze is: " + (finalPosition.getPath().length() != 0 ? finalPosition.getPath().toString() : "No Solution");
+    }
+
+    private void searchMaze(Maze maze, MazePosition currentPosition, Set<MazePosition> visitedPositions, MazePosition result) {
+        if (visitedPositions.contains(currentPosition)) {
+            return;
+        }
+        visitedPositions.add(currentPosition);
+        if (maze.isEndingPosition(currentPosition)) {
+            result.update(currentPosition);
+            return;
+        }
+        for (MazePositionChange positionChange : MazeGridDirectionConstants.getAllDirectionChanges()) {
+            MazePosition newPosition = new MazePosition(
+                    currentPosition.getRowPos() + positionChange.getRowChange(),
+                    currentPosition.getColumnPos() + positionChange.getColChange(),
+                    new StringBuilder(currentPosition.getPath()).append(positionChange.getSymbolicRepresentation()));
+            if (MazePositionUtility.isPositionTraversable(maze, newPosition) && !visitedPositions.contains(newPosition)) {
+                searchMaze(maze, newPosition, visitedPositions, result);
+            }
+        }
     }
 }
